2b6693dbf99fecf48a99a8d9b4920da5948a442e
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2023-09-05 15:59:23 +0100

    data changes

--- src/pages/ProjectDetail.tsx
+++ src/pages/ProjectDetail.tsx
@@ -49,7 +49,7 @@ const ProjectDetail = () => {
 
           <div className="flex flex-col gap-6">
             <h3 className="text-3xl font-NeutonRegular">Shots</h3>
-            <div className="grid gap-8 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 ">
+            <div className="grid gap-8 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols1 ">
               {shots?.map((item, index) => (
                 <div key={index} className="w-full h-full ">
                   <img src={item} className="w-full h-full object-contain " />

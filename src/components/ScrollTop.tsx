e72e186dba368b29fe2310f48a2b91a88f9e6a84
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2023-09-26 07:30:51 +0100

    updated scroll top button

--- src/components/ScrollTop.tsx
+++ src/components/ScrollTop.tsx
@@ -7,18 +7,16 @@ const ScrollTop = () => {
 
   let content;
 
-  if (scrollHeight > 1800) {
+  if (scrollHeight > 2500) {
     content = (
-      <div className="flex items-center justify-center fixed z-[100] h-8 w-8 rounded-full bg-purple-500 md:right-24 md:bottom-40 right-10 bottom-28 ">
-        <button
-          className="text-white"
-          onClick={() => {
-            window.scrollTo(0, 0);
-          }}
-        >
-          <FaArrowUp />
-        </button>
-      </div>
+      <button
+        className="text-white flex items-center justify-center fixed z-[100] h-8 w-8 rounded-full bg-purple-500 md:right-24 md:bottom-40 right-10 bottom-28 "
+        onClick={() => {
+          window.scrollTo(0, 0);
+        }}
+      >
+        <FaArrowUp />
+      </button>
     );
   }
 

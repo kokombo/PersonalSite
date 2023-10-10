7713ba2e2e791f66f7f07115d6467beaf6e82e38
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2023-10-10 12:16:05 +0100

    updated readme

--- src/pages/ProjectDetail.tsx
+++ src/pages/ProjectDetail.tsx
@@ -3,7 +3,6 @@ import { useParams } from "react-router-dom";
 import { projectData } from "../constant/data";
 import { projectType } from "../types/types";
 import { useState } from "react";
-import { ScrollTop } from "../components";
 
 const ProjectDetail = () => {
   const [data] = useState<projectType[] | null>(projectData);

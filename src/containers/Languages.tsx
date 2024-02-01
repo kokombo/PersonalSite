a3ad8538c453d9ac8c6b3c28986407ef92c42750
Samuel Ibrahim <samuelibrahim3029@gmail.com>
2024-02-01 10:08:45 +0100

    Added google analytics

--- src/containers/Languages.tsx
+++ src/containers/Languages.tsx
@@ -1,12 +1,12 @@
 const Languages = () => {
   return (
     <section className="languages">
-      <h2 className=" text-4xl font-NeutonBold">Techs I Play With</h2>
+      <h2 className=" text-4xl font-NeutonBold">Technical Skills</h2>
 
       <div className="languages_wrapper">
         <p>JavaScript</p>
         <p> TypeScript</p>
-        <p> React.JS</p>
+        <p> React</p>
         <p> Next.JS</p>
         <p>React Native</p>
         <p>Redux</p>
